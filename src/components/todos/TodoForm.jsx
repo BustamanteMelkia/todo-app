9bfbe1713117805b4d38e8c49f64cb3c01ee54c4
melkia bustamante <bonfil99melkia@gmail.com>
2022-01-23 22:27:09 -0600

    Validation added

--- src/components/todos/TodoForm.jsx
+++ src/components/todos/TodoForm.jsx
@@ -7,6 +7,7 @@ const TodoForm = () => {
         title: ''
     });
     const {title} = data;
+    const [hasError, setError] = useState(false);
 
     const handleChange = e=>{
         setData({
@@ -16,23 +17,28 @@ const TodoForm = () => {
     }
     const handleSubmit = e=>{
         e.preventDefault();
-        if(!title.trim())
+        if(!title.trim()){
+            setError(true);
             return;
+        }
         const newTodo = {...data, completed: false};
         addTodo(newTodo);
 
         // Reset title field
         setData({ ...data, title: ''});
+        setError(false);
     }
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <input 
-                value={title} 
-                name='title'
-                className='form__control' 
-                onChange={handleChange} 
-                placeholder='Enter a new todo'
-            />
+        <form className={`form`} onSubmit={handleSubmit}>
+            <div className={`form__group ${hasError?'form__group--error':''}`}>
+                <input 
+                    value={title} 
+                    name='title'
+                    className='form__control' 
+                    onChange={handleChange} 
+                    placeholder='Enter a new todo'
+                />
+            </div>
             <button type='submit' className='form__submit'>
                 <img src={addIcon} alt="add icon" />
             </button>
